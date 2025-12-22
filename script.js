3b01684a066a674107bb3ace01435aa24de2a532
Leo  Zuze <leonoelzuze@gmail.com>
2025-12-22 23:55:37 +0530

    Update script.js

--- script.js
+++ script.js
@@ -46,4 +46,16 @@ document.addEventListener("DOMContentLoaded", function() {
   
 });
 
+//new
+
+const nav = document.querySelector(".nav");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 50) {
+    nav.classList.add("scrolled");
+  } else {
+    nav.classList.remove("scrolled");
+  }
+});
+
 
